abd9b618eed9b8a51165467341f485b22e8d9b9d
Sully's Mac <sully@Sullyss-MacBook-Pro.local>
2021-04-05 12:45:47 -0700

    mvp

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+  return HttpResponse('<h1>Hello </h1>')
 
 # Create your views here.
+def about(request):
+    return render(request, 'about.html')
